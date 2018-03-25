1fac9f618e74d598f11352d9ca98be44c267eb5d
mocca <moccababy777@gmail.com>
2018-03-26 00:35:43 +1300

    modified arrow

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
           $(".calculate__details").next().slideUp('slow');
 	  $(this).next().slideToggle();
 	  var x=$(this).find(".arrow").eq(0);
-          if(x.hasClass("arrow-up")){
+          if(x.hasClass("arrow-down")){
 	    x.removeClass("arrow-up").addClass("arrow-down");
 	  }else{
 		if(x.hasClass("arrow-down")){
